7f41a8669fe1e8c1774c2d021e0487fc90ea9a52
Dominik Voscek <dodovoscek@gmail.com>
2021-02-23 20:20:21 +0100

    Init commit

---
+++ OLC Networking/main.cpp
@@ -0,0 +1,18 @@
+// defining windows 10
+#ifdef _WIN32
+#define _WIN32_WINNT 0x0A00
+#endif
+
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
+
+int main()
+{
+	asio::error_code ec;
+
+	// platform specific interface
+	asio::io_context context;
+
+	return 0;
+}
