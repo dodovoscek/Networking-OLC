38c3e29bc6f8625fc1d25da461f44ff90f0df3fc
Dominik Voscek <dodovoscek@gmail.com>
2021-02-24 14:44:19 +0100

    Basic socket functionality

--- OLC Networking/main.cpp
+++ OLC Networking/main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 // defining windows 10
 #ifdef _WIN32
 #define _WIN32_WINNT 0x0A00
@@ -14,5 +16,44 @@ int main()
 	// platform specific interface
 	asio::io_context context;
 
+	// address with a port where we want to connect to
+	asio::ip::tcp::endpoint endpoint(asio::ip::make_address("93.184.216.34", ec), 80);
+
+	// context delivers the implementation
+	asio::ip::tcp::socket socket(context);
+	socket.connect(endpoint, ec);
+
+	if (!ec)
+	{
+		std::cout << "Connected" << std::endl;
+	}
+	else
+	{
+		std::cout << "Failed to connect to address:" << ec.message() << std::endl;
+	}
+
+	if (socket.is_open())
+	{
+		std::string sRequest =
+			"GET /index.html HTTP/1.1\r\n"
+			"Host: example.com\r\n"
+			"Connection: close\r\n\r\n";
+
+		socket.write_some(asio::buffer(sRequest.data(), sRequest.size()), ec);
+
+		size_t bytes = socket.available();
+		std::cout << "Bytes available: " << bytes << std::endl;
+
+		if (bytes > 0)
+		{
+			std::vector<char> vBuffer(bytes);
+			socket.read_some(asio::buffer(vBuffer.data(), vBuffer.size()), ec);
+
+			for (auto c : vBuffer)
+				std::cout << c;
+		}
+	}
+
+	system("pause");
 	return 0;
 }
